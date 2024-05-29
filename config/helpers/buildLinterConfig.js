1406e4521266e490b3a740c0bcda4eb9a81bea6e
Nick Snyder <nick@nicksnyder.is>
2024-05-29 09:43:05 -0700

    Update Prettier and Markdownlint build scripts

--- config/helpers/buildLinterConfig.js
+++ config/helpers/buildLinterConfig.js
@@ -1,80 +1,114 @@
-// TODO Add documentation header.
-// TODO Organize variables.
+/**
+ * --------------------------------------------------------------------------
+ *   Helpers → Build Linter Configs
+ *   Compiles linter rules into a single config file.
+ * --------------------------------------------------------------------------
+*/
 
-const fs = require('fs');
-const glob = require('glob');
-const _ = require('lodash');
 
+
+/**
+ *   Imports and variables
+ * ------------------------------------------------------ */
+const { globSync } = require('glob');
+const { mergeWith } = require('lodash');
+const { copyFileSync, writeFileSync } = require('fs');
+
+// Variables
 const dotfilesPath = process.cwd();
-const lintersPath = `${dotfilesPath}/resources/config/linters`;
+const lintersPath = `${dotfilesPath}/config/linters`;
 
-const storeData = (data, path, ext) => {
-	if (ext === 'json') {
-		fs.writeFileSync(String(path), JSON.stringify(data));
-	} else {
-		fs.writeFileSync(String(path), data);
-	}
-};
 
-const buildLinterConfig = options => {
-	let rulesMerged = {};
-	const matchingFiles = glob.sync(String(options.pathToRules));
-
-	matchingFiles.forEach(file => {
-		rulesMerged = _.mergeWith(rulesMerged,
-			require(String(file)),
-			customizer);
-	});
-
-	return rulesMerged;
-};
-
+/**
+ *   Methods
+ * ------------------------------------------------------ */
+// Properly merges arrays.
 const customizer = (objValue, srcValue) => {
 	if (Array.isArray(objValue)) {
 		return objValue.concat(srcValue);
 	}
 };
 
-// ESLint
-const eslint = () => {
-	const fileName = '.eslintrc';
-	const eslintOptions = buildLinterConfig({ pathToRules: `${lintersPath}/eslint/{rules,plugins}/*.js` });
-	storeData(eslintOptions, `${dotfilesPath}/${fileName}`, 'json');
+
+// Merges all configuration files into a single object.
+const mergeConfigFiles = (options) => {
+	const matchingFiles = globSync(String(options.pathToRules));
+	let rulesMerged = {};
+	matchingFiles.forEach((file) => {
+		const ruleSet = require(String(file));
+		rulesMerged = mergeWith(rulesMerged, ruleSet, customizer);
+	});
+	return rulesMerged;
 };
+
+
+// Moves ignore file.
+const moveIgnoreFile = (source, destination) => {
+	copyFileSync(String(source), String(destination));
+};
+
+// Saves configuration to file.
+const saveToFile = (data, path, ext) => {
+	if (ext === 'json') {
+		writeFileSync(String(path), JSON.stringify(data));
+	} else {
+		writeFileSync(String(path), data);
+	}
+};
+
+
+/**
+ *   Configs
+ * ------------------------------------------------------ */
+
+// Prettier
+const prettier = () => {
+	const fileName = '.prettierrc';
+	const prettierOptions = mergeConfigFiles({ pathToRules: `${lintersPath}/prettier/{rules,overrides,plugins}/*.js` });
+	saveToFile(prettierOptions, `${dotfilesPath}/${fileName}`, 'json');
+	moveIgnoreFile(`${lintersPath}/prettier/.prettierignore`, `${dotfilesPath}/.prettierignore`);
+};
+
+
+
+// // ESLint
+// const eslint = () => {
+// 	const fileName = '.eslintrc';
+// 	const eslintOptions = buildLinterConfig({ pathToRules: `${lintersPath}/eslint/{rules,plugins}/*.js` });
+// 	saveToFile(eslintOptions, `${dotfilesPath}/${fileName}`, 'json');
+// };
 
 // Markdownlint.
 const markdownlint = () => {
 	const fileName = '.markdownlint.json';
-	const markdownlintOptions = buildLinterConfig({ pathToRules: `${lintersPath}/markdownlint/*.js` });
-	storeData(markdownlintOptions, `${dotfilesPath}/${fileName}`, 'json');
+	const markdownlintOptions = buildLinterConfig({ pathToRules: `${lintersPath}/markdownlint/rules/*.js` });
+	saveToFile(markdownlintOptions, `${dotfilesPath}/${fileName}`, 'json');
 };
 
-// PHP-CS-Fixer.
-const phpCsFixer = () => {
-	const fileName = '.php-cs-fixer-rules.json';
-	const rules = buildLinterConfig({ pathToRules: `${lintersPath}/php-cs-fixer/**/*.js` });
-	storeData(rules, `${dotfilesPath}/${fileName}`, 'json');
-};
+// // PHP-CS-Fixer.
+// const phpCsFixer = () => {
+// 	const fileName = '.php-cs-fixer-rules.json';
+// 	const rules = buildLinterConfig({ pathToRules: `${lintersPath}/php-cs-fixer/**/*.js` });
+// 	saveToFile(rules, `${dotfilesPath}/${fileName}`, 'json');
+// };
 
-// Prettier
-const prettier = () => {
-	// Yes, I know Prettier isn't technically a linter. Get off my back. XOXO — Nick
-	const fileName = '.prettierrc';
-	const prettierOptions = buildLinterConfig({ pathToRules: `${lintersPath}/prettier/{rules,overrides}/*.js` });
-	storeData(prettierOptions, `${dotfilesPath}/${fileName}`, 'json');
-};
+// // Prettier
+// const prettier = () => {
+// 	// Yes, I know Prettier isn't technically a linter. Get off my back. XOXO — Nick
+// 	const fileName = '.prettierrc';
+// 	const prettierOptions = buildLinterConfig({ pathToRules: `${lintersPath}/prettier/{rules,overrides}/*.js` });
+// 	saveToFile(prettierOptions, `${dotfilesPath}/${fileName}`, 'json');
+// };
 
-// Stylelint
-const stylelint = () => {
-	const fileName = '.stylelintrc.json';
-	const stylelintOptions = buildLinterConfig({ pathToRules: `${lintersPath}/stylelint/rules/**/*.js` });
-	storeData(stylelintOptions, `${dotfilesPath}/${fileName}`, 'json');
-};
+// // Stylelint
+// const stylelint = () => {
+// 	const fileName = '.stylelintrc.json';
+// 	const stylelintOptions = buildLinterConfig({ pathToRules: `${lintersPath}/stylelint/rules/**/*.js` });
+// 	saveToFile(stylelintOptions, `${dotfilesPath}/${fileName}`, 'json');
+// };
+
 
 module.exports = {
-	eslint,
 	markdownlint,
-	phpCsFixer,
-	prettier,
-	stylelint
+	prettier
 };
