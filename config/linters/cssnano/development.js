300d284fec2da85d04af8303c0d40d2e023e2617
Nick Snyder <nick@nicksnyder.is>
2024-07-15 09:43:57 -0700

    Add CSSNano

---
+++ config/linters/cssnano/development.js
@@ -0,0 +1,15 @@
+/**
+ * --------------------------------------------------------------------------
+ *   Linters → CSSNano → Rules → Development
+ *   @see https://cssnano.co/docs/optimisations
+ * --------------------------------------------------------------------------
+*/
+
+module.exports = {
+	discardComments: false,
+	discardOverridden: false,
+	discardUnused: false,
+	minifyParams: false,
+	minifySelectors: false,
+	normalizeWhitespace: false
+};
